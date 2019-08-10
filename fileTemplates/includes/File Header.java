9c5b1a5915962b312d1d7995a5233b03ca474b72
JasonCui <531318803@qq.com>
2019-08-10 20:56:44 +0800

    IU-2019.1.2 <jasoncui@JasoncuideMacBook-Pro.local Create markdown.xml, IntelliLang.xml, project.default.xml, diff.xml, Default.xml, ignore.xml, find.xml, File Header.java, gradle.run.settings.xml, _@user_Darcula.icls, path.macros.xml, mavenVersion.xml, javarebel.xml, Eclipse _Mac OS X Jason_C_.xml, Default.xml, docker.xml, keymap.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, _@user_Default.icls, lombok-plugin.xml, git.xml, terminal.xml, ActionScript File Header.as, ui.lnf.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, remote-servers.xml, myTemplate.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, github.xml, templates.xml, jdk.table.xml, mavenRunHelper.xml, intentionSettings.xml, laf.xml, web-browsers.xml, vcs.xml, codota.xml, databaseSettings.xml, docker-registry.xml, toStringTemplates.xml, mybatis.xml, usageView.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,10 @@
+/**
+ * @Project : ${PROJECT_NAME}
+ * @Package Name  : ${PACKAGE_NAME}
+ * @Class Name    : ${CLASS_NAME}
+ * @Description : 
+ * @Author : Jason.C
+ * @Creation Date : ${YEAR}年${MONTH}月${DAY}日 ${TIME}
+ * @ModificationHistory Who        When          What
+ * --------   ----------    -----------------------------------
+ */
